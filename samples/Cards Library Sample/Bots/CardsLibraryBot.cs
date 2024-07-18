20a8eba28f134f2dee5b47d7685edc323eab855f
Jagadeesh_Paranji <jagadeesh.paranji@dell.com>
2024-07-18 11:40:43 +0530

    updated the code

--- samples/Cards Library Sample/Bots/CardsLibraryBot.cs
+++ samples/Cards Library Sample/Bots/CardsLibraryBot.cs
@@ -7,7 +7,7 @@ using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
-using System.Threading.Tasks;
+\
 
 namespace Cards_Library_Sample.Bots
 {
